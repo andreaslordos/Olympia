7ec514f457d25cea157f3bb07c168820a7b50faf
Andreas Lordos <andreasglordos@gmail.com>
2017-11-14 22:40:48 +0200

    Update xkcd_viewer.py

--- code/xkcd_viewer.py
+++ code/xkcd_viewer.py
@@ -1,12 +1,16 @@
 def xkcd():
     from PIL import Image
     import xkcd
-    import os
-    os.chdir("C:\\Users\\user\\Desktop\\Smart Mirror Github")
+    from os import chdir
+    dirFile=open("dir.txt","r")
+    directory=dirFile.read()
+    dirFile.close()
     comic=xkcd.getRandomComic()
-    comic.download(output='C:\\Users\\user\\Desktop\\Smart Mirror Github',outputFile='xkcd.jpg',silent=False)
+    comic.download(output=directory+'\\resources',outputFile='xkcd.jpg',silent=False)
+    chdir(directory+"\\resources")
     img=Image.open('xkcd.jpg')
     img.show()
+    chdir(directory+"\\code")
     return img
     
     
