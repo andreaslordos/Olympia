2c37e7fa8594590eac7551c7d1650f5b7a1f9095
Andreas Lordos <andreasglordos@gmail.com>
2017-11-27 21:26:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,14 @@ dir=getcwd()
 
 if operating_system=="linux" or operating_system=="Darwin":
     f1=open("code/dir.txt","w")
+    f2=open("resources/dir.txt","w")
 elif operating_system[0:3]=="win":
     f1=open("code\\dir.txt","w")
+    f2=open("resources\\dir.txt","w")
     
 f1.write(dir)
+f2.write(dir)
 f1.close()
+f2.close()
 
 print("Setup complete.")
