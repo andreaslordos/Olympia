f55d0aac122555b760ffc64bbeec0a3a72f11137
Andreas Lordos <andreasglordos@gmail.com>
2017-12-11 00:27:15 +0200

    Update chance.py

--- code/chance.py
+++ code/chance.py
@@ -1,3 +1,7 @@
+'''
+Input: userinput
+Output: heads/tails/you rolled a [number]
+'''
 def Chance(userinput):
     from random import randint
     if "coin" in userinput.lower():
@@ -6,7 +10,7 @@ def Chance(userinput):
             return "heads"
         else:
             return "tails"
-    else:
+    else: #roll a die feature
         userinput=userinput.split()
         for word in userinput:
             if word[0].lower()=="d":
