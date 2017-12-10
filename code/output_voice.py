9dcf5844d180fcbcf15803dff38f0d8780d06c9f
Andreas Lordos <andreasglordos@gmail.com>
2017-12-10 14:10:04 +0200

    Fixed output.mp3 bug

--- code/output_voice.py
+++ code/output_voice.py
@@ -1,10 +1,8 @@
 from gtts import gTTS as tts
-from tinytag import TinyTag as tt
-from pygame import mixer
-from time import sleep
 from os import chdir
-from os import remove
-from os import getcwd
+from tinytag import TinyTag as tt
+from time import sleep
+import subprocess
 dirFile=open("dir.txt","r")
 directory=dirFile.read()
 dirFile.close()
@@ -17,19 +15,9 @@ def voiceOutput(textToSay):
         fullstr=fullstr[:-1]
         whatToSay=tts(text=fullstr,lang='en')
         whatToSay.save("output.mp3")
-        mixer.init()
-        mixer.music.load("output.mp3")
-        mixer.music.play()
-        try:
-            chdir(directory+"\\resources")
-            audio=tt.get("output.mp3")
-        except:
-            print(getcwd())
-            chdir(directory+"\\code")
-            audio=tt.get("output.mp3")
+        audio=tt.get("output.mp3")
+        subprocess.run("output.mp3",shell=True)
         sleep(audio.duration)
-        mixer.music.stop()
-        mixer.quit()
     except ConnectionError:
         print("connection error")
     return
